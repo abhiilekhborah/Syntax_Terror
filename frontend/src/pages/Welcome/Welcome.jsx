44f613a79df420c13417ef33dd5e4f62fc40feda
Koustab Saikia <koustavsaikia63@gmail.com>
2026-03-13 22:11:22 +0530

    Add welcome page

---
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,68 @@
+import styles from './Welcome.module.css'
+import homeIcon from '../../assets/home.svg'
+import buildIcon from '../../assets/build.svg'
+
+function Welcome() {
+
+    const heading = {
+        color: "rgb(249, 115, 22)"
+    }
+
+    return(
+        <div className={styles.background}>
+            <div className={styles.civicPlatformCaption}>
+                <div></div>
+                SMART CIVIC PLATFORM
+            </div>
+
+            <div className={styles.heading}>
+                <div>Report. Track.</div>
+                <div style={heading}>Resolve.</div>
+            </div>
+
+            <div className={styles.subheading}>
+                NagarSetu connects citizens and local authorities to fix civic issues — from potholes to broken streetlights — faster than ever.
+            </div>
+
+            <div className={styles.cardContainer}>
+                <div className={styles.cards} id="card1">
+                    <div className={styles.cardIcon}>
+                        <img src={homeIcon} alt="home" height={35} width={35} />
+                    </div>
+
+                    <div className={styles.cardTitle}>
+                        Citizen Portal
+                    </div>
+
+                    <div className={styles.cardText}>
+                        Report issues, track progress and vote on community problems.
+                    </div>
+
+                    <div className={styles.cardEnter}>
+                        Click to enter
+                    </div>
+                </div>
+
+                <div className={styles.cards} id="card2">
+                    <div className={styles.cardIcon}>
+                        <img src={buildIcon} alt="build" height={35} width={35} />
+                    </div>
+
+                    <div className={styles.cardTitle}>
+                        Authority Portal
+                    </div>
+
+                    <div className={styles.cardText}>
+                        Manage assigned tasks, update status and close resolve issue.
+                    </div>
+
+                    <div className={styles.cardEnter}>
+                        Click to enter
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Welcome
