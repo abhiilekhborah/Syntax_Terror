70cabe26d235ee5adc39183c7f9a316bfebb3568
Koustab Saikia <koustavsaikia63@gmail.com>
2026-03-13 23:15:58 +0530

    Add the grid background

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -1,6 +1,5 @@
 import styles from './Welcome.module.css'
 import homeIcon from '../../assets/home.svg'
-import buildIcon from '../../assets/build.svg'
 
 function Welcome() {
 
@@ -18,7 +17,7 @@ function Welcome() {
             </div>
 
             <div className={styles.heading}>
-                <div>Report. Track.</div>
+                <div className={styles.headingTop}>Report. Track.</div>
                 <div style={heading}>Resolve.</div>
             </div>
 
@@ -38,24 +37,6 @@ function Welcome() {
 
                     <div className={styles.cardText}>
                         Report issues, track progress and vote on community problems.
-                    </div>
-
-                    <div className={styles.cardEnter}>
-                        Click to enter
-                    </div>
-                </div>
-
-                <div className={styles.cards} id="card2">
-                    <div className={styles.cardIcon}>
-                        <img src={buildIcon} alt="build" height={35} width={35} />
-                    </div>
-
-                    <div className={styles.cardTitle}>
-                        Authority Portal
-                    </div>
-
-                    <div className={styles.cardText}>
-                        Manage assigned tasks, update status and close resolve
                     </div>
 
                     <div className={styles.cardEnter}>
